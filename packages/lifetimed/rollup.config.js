2f4be41db91fcfa28413a94b5ff57205cc790b54
VladV <26966368+vanifatovvlad@users.noreply.github.com>
2025-07-27 12:22:38 +0300

    Add rollup-plugin-dts to generate typescript definition file

--- packages/lifetimed/rollup.config.js
+++ packages/lifetimed/rollup.config.js
@@ -1,10 +1,18 @@
 import typescript from '@rollup/plugin-typescript';
+import { dts } from 'rollup-plugin-dts';
 
-export default {
-    input: 'src/index.ts',
-    output: {
-        dir: 'dist',
-        format: 'esm',
+export default [
+    {
+        input: 'src/index.ts',
+        output: {
+            dir: 'dist',
+            format: 'esm',
+        },
+        plugins: [typescript()],
     },
-    plugins: [typescript()],
-};
+    {
+        input: './dist/temp/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts()],
+    },
+];
